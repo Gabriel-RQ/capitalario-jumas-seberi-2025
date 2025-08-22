8b2c7e4c6c616f30abbee6248e91ebdffa02e92c
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-08-22 17:07:37 -0300

    update(counter): makes the counter increment up until the max value

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -21,6 +21,11 @@ export async function load({ locals }) {
 
 export const actions = {
   increment: async () => {
-    await incrementCounter();
+    const max = await getMaxCounter();
+    const counter = await getCounter();
+
+    if (counter < max) {
+      await incrementCounter();
+    }
   },
 };
