066f14322766fd236320b6c22d82f38387fa892d
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-08-22 15:23:25 -0300

    fix(modal): fixes banner not displaying on Modal

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { XIcon } from "@lucide/svelte";
   import { fade, scale } from "svelte/transition";
-  import { Link } from "./io";
   import type { Snippet } from "svelte";
 
   type ModalProps = {
@@ -21,10 +20,12 @@
 
 <div
   class="z-20 fixed top-1/2 left-1/2 -translate-1/2 bg-glass-dark border border-glass p-4 rounded-lg min-w-[340px] max-w-9/10 w-max md:w-[424px] lg:w-[564px] backdrop-blur-lg grid grid-rows-[150px_1fr_auto] gap-y-3"
+  style="--bannerUrl: url('{banner}')"
   transition:scale={{ duration: 200 }}
 >
   <div
-    class="text-center inline-flex gap-2 justify-between w-full before:absolute before:inset-0 before:h-40 {`before:bg-[url(${banner})]`} before:bg-cover before:opacity-25 before:rounded-lg *:z-10"
+    id="modal-header"
+    class="text-center inline-flex gap-2 justify-between w-full before:absolute before:inset-0 before:h-40 before:bg-cover before:opacity-25 before:rounded-t-lg *:z-10"
   >
     <div class="w-9"></div>
     <h3 class="font-montserrat font-bold text-2xl">{title}</h3>
@@ -39,3 +40,9 @@
 
   {@render cta?.()}
 </div>
+
+<style>
+  #modal-header::before {
+    background-image: var(--bannerUrl);
+  }
+</style>
